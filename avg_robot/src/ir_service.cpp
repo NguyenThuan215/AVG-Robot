e20b49201076d82fe29f37d89fd6d63aad63a9fc
unknown <ngthuanbb1102@gmail.com>
2021-12-22 21:41:14 +0700

    update code

---
+++ avg_robot/src/ir_service.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+#include "avg_robot/IRSignal.h"
+#include <string.h>
+   
+bool avg(avg_robot::IRSignal::Request  &req, avg_robot::IRSignal::Response &res) {
+    ROS_INFO("request: IR_1=%d, IR_2=%d, IR_3=%d, IR_4=%d, IR_5=%d", req.IR_1, req.IR_2, req.IR_3, req.IR_4, req.IR_5);
+
+    if (req.IR_2 && req.IR_3 && req.IR_4) {
+        if (req.IR_1 && req.IR_5) res.state = "Fork/Crossroad";
+        else if (req.IR_1 && !req.IR_5) res.state = "Fork/LeftTurn";
+        else if (req.IR_5 && !req.IR_1) res.state = "Fork/RightTurn";
+        else res.state = "StraightLine";
+    } else if (req.IR_2 && req.IR_3 && !req.IR_4) {
+        res.state = "RightSkew";
+    } else if (!req.IR_2 && req.IR_3 && req.IR_4) {
+        res.state = "LeftSkew";
+    } else if (!req.IR_3) {
+        res.state = "MissedLine";
+    } else {
+        res.state = "ERR";
+    }
+
+    ROS_INFO("sending back response - Direction: [%s]", res.state.c_str());
+    return true;
+}
+   
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ir_server");
+    ros::NodeHandle n;
+    ros::ServiceServer service = n.advertiseService("ir_signal", avg);
+    ROS_INFO("Ready to recieve IR.");
+    ros::spin();
+
+    return 0;
+}
---
+++ avg_robot/src/keyboard_subscriber.cpp
@@ -0,0 +1,70 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "avg_robot/Navigation.h"
+#include "avg_robot/MotorWarning.h"
+#include "avg_robot/MotorControl.h"
+#include <algorithm>
+#include <string>
+
+bool canMove = true;
+
+std::string controlRobot = "";
+
+std::string change = "";
+
+void chatterCallback(const avg_robot::Navigation navi)
+{
+
+  if (canMove) {
+    ROS_INFO("receive direction: [%s]", navi.navigation.c_str());
+    change = navi.navigation.c_str();
+    
+    std::transform(change.begin(), change.end(),change.begin(), ::toupper);
+
+    controlRobot = change;
+  } else {
+    ROS_INFO("Robot stop!");
+    controlRobot="STOP";
+  }
+  
+
+}
+
+void warningCallback(const avg_robot::MotorWarning warning)
+{
+
+  // ROS_INFO("receive warning: [%d]", warning.enable);
+  if (warning.enable) {
+    ROS_INFO("GO!");
+    canMove = true;
+  } else {
+    ROS_INFO("CANCEL!");
+    canMove = false;
+    controlRobot="STOP";
+  }
+
+}
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "keyboard_robot_subscriber");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("input_keyboard", 1, chatterCallback);
+  ros::Subscriber motor_sub = n.subscribe("motor_warning", 1, warningCallback);
+  ros::Publisher chatter_pub = n.advertise<avg_robot::MotorControl>("motor_control", 1000);
+  ros::Rate loop_rate(1);
+
+  while (ros::ok()) {
+      avg_robot::MotorControl controlMsg;
+      controlMsg.control = controlRobot;
+      ROS_INFO("send Control to motor [%s]", controlMsg.control.c_str());
+      chatter_pub.publish(controlMsg);
+      ros::spinOnce();
+      loop_rate.sleep();
+  }
+    
+  ros::spin();
+
+  return 0;
+}
